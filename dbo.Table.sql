a79bb323810f0a91a49c0f7aa427d3cd6782af4f
Dinupa Karunaarachchi <86536976+DinupaKarunaarachchi@users.noreply.github.com>
2021-06-27 16:11:45 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Satellites]
+(
+	[satid] INT NOT NULL PRIMARY KEY, 
+    [satname] NCHAR(10) NULL, 
+    [longtitute] NCHAR(10) NULL, 
+    [latitute] NCHAR(10) NULL, 
+    [elevation] NCHAR(10) NULL, 
+    [health] NCHAR(10) NULL
+)
