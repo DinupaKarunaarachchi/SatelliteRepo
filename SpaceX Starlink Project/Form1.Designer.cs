9eb31f06456b44996235d36b49c84e792a8da7d0
Dinupa Karunaarachchi <86536976+DinupaKarunaarachchi@users.noreply.github.com>
2021-06-27 16:11:28 +0530

    Add files via upload

---
+++ SpaceX Starlink Project/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace SpaceX_Starlink_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDlabel = new System.Windows.Forms.Label();
+            this.namelabel = new System.Windows.Forms.Label();
+            this.longitudelabel = new System.Windows.Forms.Label();
+            this.elevationlabel = new System.Windows.Forms.Label();
+            this.latitudelabel = new System.Windows.Forms.Label();
+            this.healthstatuslabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IDlabel
+            // 
+            this.IDlabel.AutoSize = true;
+            this.IDlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDlabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.IDlabel.Location = new System.Drawing.Point(13, 25);
+            this.IDlabel.Name = "IDlabel";
+            this.IDlabel.Size = new System.Drawing.Size(108, 24);
+            this.IDlabel.TabIndex = 0;
+            this.IDlabel.Text = "Satellite ID";
+            this.IDlabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // namelabel
+            // 
+            this.namelabel.AutoSize = true;
+            this.namelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namelabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.namelabel.Location = new System.Drawing.Point(13, 72);
+            this.namelabel.Name = "namelabel";
+            this.namelabel.Size = new System.Drawing.Size(139, 24);
+            this.namelabel.TabIndex = 1;
+            this.namelabel.Text = "Satelite Name";
+            // 
+            // longitudelabel
+            // 
+            this.longitudelabel.AutoSize = true;
+            this.longitudelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.longitudelabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.longitudelabel.Location = new System.Drawing.Point(13, 118);
+            this.longitudelabel.Name = "longitudelabel";
+            this.longitudelabel.Size = new System.Drawing.Size(103, 24);
+            this.longitudelabel.TabIndex = 2;
+            this.longitudelabel.Text = "Longitude";
+            this.longitudelabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // elevationlabel
+            // 
+            this.elevationlabel.AutoSize = true;
+            this.elevationlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.elevationlabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.elevationlabel.Location = new System.Drawing.Point(13, 167);
+            this.elevationlabel.Name = "elevationlabel";
+            this.elevationlabel.Size = new System.Drawing.Size(96, 24);
+            this.elevationlabel.TabIndex = 3;
+            this.elevationlabel.Text = "Elevation";
+            this.elevationlabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // latitudelabel
+            // 
+            this.latitudelabel.AutoSize = true;
+            this.latitudelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.latitudelabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.latitudelabel.Location = new System.Drawing.Point(397, 118);
+            this.latitudelabel.Name = "latitudelabel";
+            this.latitudelabel.Size = new System.Drawing.Size(83, 24);
+            this.latitudelabel.TabIndex = 4;
+            this.latitudelabel.Text = "Latitude";
+            // 
+            // healthstatuslabel
+            // 
+            this.healthstatuslabel.AutoSize = true;
+            this.healthstatuslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthstatuslabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.healthstatuslabel.Location = new System.Drawing.Point(368, 163);
+            this.healthstatuslabel.Name = "healthstatuslabel";
+            this.healthstatuslabel.Size = new System.Drawing.Size(132, 24);
+            this.healthstatuslabel.TabIndex = 5;
+            this.healthstatuslabel.Text = "Health Status";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(174, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 27);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(174, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(534, 27);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(174, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 27);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(174, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 27);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(535, 115);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 27);
+            this.textBox5.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(363, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 34);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Find Satellite";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PowderBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(17, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PowderBlue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(267, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 31);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PowderBlue;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(513, 247);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(195, 31);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(535, 162);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 28);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 309);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(691, 246);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(267, 576);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 35);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Clear All";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 638);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.healthstatuslabel);
+            this.Controls.Add(this.latitudelabel);
+            this.Controls.Add(this.elevationlabel);
+            this.Controls.Add(this.longitudelabel);
+            this.Controls.Add(this.namelabel);
+            this.Controls.Add(this.IDlabel);
+            this.Name = "Form1";
+            this.Text = "SpaceX Starlink Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IDlabel;
+        private System.Windows.Forms.Label namelabel;
+        private System.Windows.Forms.Label longitudelabel;
+        private System.Windows.Forms.Label elevationlabel;
+        private System.Windows.Forms.Label latitudelabel;
+        private System.Windows.Forms.Label healthstatuslabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ SpaceX Starlink Project/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SpaceX_Starlink_Project
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\spacex.mdf;Integrated Security=True;Connect Timeout=30");
+       
+        int ID;
+        string name;
+        string longitude;
+        string elevation;
+        string latitude;
+        string healthstatus;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            healthstatus = comboBox1.SelectedItem.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            ID = int.Parse(textBox1.Text);
+            name = textBox2.Text;
+            longitude = Convert.ToString(textBox3.Text);
+            elevation = Convert.ToString(textBox4.Text);
+            latitude = Convert.ToString(textBox5.Text);
+
+
+
+            string query = "INSERT INTO Satellites (satid,satname,longtitute,latitute,elevation,health) VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox5.Text + "','" + textBox4.Text + "','" + comboBox1.Text + "')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successful");
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show("" + se.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            display();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Excelent");
+            comboBox1.Items.Add("Good");
+            comboBox1.Items.Add("Weak");
+        }
+
+        public void display()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM SATELLITES",con);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM Satellites WHERE satid = "+ textBox1.Text + "";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted Successfully");
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(se.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            display();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Satellites SET satname='" + textBox2.Text + "',longtitute='" + textBox3.Text + "',latitute='" + textBox5.Text + "',elevation='" + textBox4.Text + "',health='" + comboBox1.Text + "' WHERE satid='" + textBox1.Text + "' ",con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            display();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM Satellites";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successful");
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show("Error......" + se.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            display();
+        }
+    }
+}
